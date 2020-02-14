a6a880569581a291c5e1d06f06af60f42613cddd
古賀 靖典 <koga@koganoMac-mini.local>
2020-02-14 11:18:57 +0900

    create initial files

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer id="footer" class="footer">
+  <div class="footer-inner">
+ 
+  </div><!--end footer-inner-->
+</footer>
+<?php wp_footer(); ?><!--システム・プラグイン用-->
+</body>
+</html>
+
---
+++ sideber.php
@@ -0,0 +1,6 @@
+<aside id="sidebar" class="sidebar">
+  <div class="sidebar-inner">
+ 
+  </div><!--end sidebar-inner-->
+</aside>
+
